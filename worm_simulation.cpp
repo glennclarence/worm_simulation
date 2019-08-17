7a9d7152bad26610ab73141a2aaa4ff8b709c730
glenn <glenn.glashagen@gmail.com>
2019-08-17 09:42:28 +0200

    added commentry

--- worm_simulation.cpp
+++ worm_simulation.cpp
@@ -7,11 +7,9 @@ namespace py = pybind11;
 
 
 PYBIND11_MODULE(worm_simulation, m) {
+        m.doc() = "Class to simulate a 3 dimensional worm moving inside a box."; 
     py::class_<WormSpace::WormSimulation>(m, "worm_simulation")
-        .def(py::init<int, int>())
-        .def("run", &WormSpace::WormSimulation::run,
-         py::return_value_policy::move);
+        .def(py::init<int, int>(), "Create an instance of worm_simulation, via its size and the box dimenstion given as integer.",py::arg("worm_size"), py::arg("box_dimension"))
+        .def("run", &WormSpace::WormSimulation::run,"Run the simulation with a specified number of iterations and increments that determines the number of positions returned as a numpy array",
+        py::return_value_policy::move);
 }
-
-
-
