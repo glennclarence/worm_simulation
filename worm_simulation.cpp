1e62b568ecd2072df53a375f5ff4555eb3a36ba8
glenn <glenn.glashagen@gmail.com>
2019-08-17 09:54:05 +0200

    added commentry to run method

--- worm_simulation.cpp
+++ worm_simulation.cpp
@@ -11,5 +11,5 @@ PYBIND11_MODULE(worm_simulation, m) {
     py::class_<WormSpace::WormSimulation>(m, "worm_simulation")
         .def(py::init<int, int>(), "Create an instance of worm_simulation, via its size and the box dimenstion given as integer.",py::arg("worm_size"), py::arg("box_dimension"))
         .def("run", &WormSpace::WormSimulation::run,"Run the simulation with a specified number of iterations and increments that determines the number of positions returned as a numpy array",
-        py::return_value_policy::move);
+        py::return_value_policy::move, py::arg("n_steps"),py::arg("return_increment"),py::arg("update_random_joints") );
 }
